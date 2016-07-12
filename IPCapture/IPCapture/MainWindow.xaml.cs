df52d3ceb00f8cf50221a4d3ffbf1a166151e876
AHW1SCP <Will.Ashworth@uk.bosch.com>
2016-07-12 17:00:28 +0100

    PropertyChanged event almost working

--- IPCapture/IPCapture/MainWindow.xaml.cs
+++ IPCapture/IPCapture/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ using System.Net.NetworkInformation;
 using System.Collections.ObjectModel;
 using System.Reflection;
 
-namespace IPCapture
+namespace IPCapture 
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
