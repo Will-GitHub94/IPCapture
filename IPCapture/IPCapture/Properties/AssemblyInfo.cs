bac54e98c74259664ea027a9412c433613be2713
AHW1SCP <Will.Ashworth@uk.bosch.com>
2016-07-13 15:42:07 +0100

    Update README

--- IPCapture/IPCapture/Properties/AssemblyInfo.cs
+++ IPCapture/IPCapture/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Managed Wifi API")]
-[assembly: AssemblyDescription(".NET managed wrapper for Windows Native Wifi API.")]
+[assembly: AssemblyTitle("Network Capture")]
+[assembly: AssemblyDescription("Small GUI widget to display devices current network information")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Monfort Software Engineering")]
-[assembly: AssemblyProduct("Managed Wifi API")]
-[assembly: AssemblyCopyright("Copyright © Monfort Software Engineering 2007")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Network Capture")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# IPCapture
+# Network Capture
+
+Small GUI widget to display devices current network information
+
+Fields shown:
+
+# Network
+- Network Connection
+- Network Connection Type
+- SSID
+- Default Gateway
+- Internet Connection
+- External IP
+
+# Machine
+- IPv4
+- (Local) IPv6
+- MAC Address
+- Subnet Mask
+- Machine Name
+- Operating System
+- OS Architecture
+- OS Manufacturer
+
+Current supported platforms:
+- Windows
